6ccafba722bb5ee64dea9f036c8e1a000b7eb0d0
jyliu17 <jyliu17@gmail.com>
2020-11-09 23:41:59 -0500

    welcome page

---
+++ lib/models/cli.rb
@@ -0,0 +1,28 @@
+require 'pry'
+require 'tty-prompt'
+
+class CLI
+
+    def welcome
+        
+        puts dumbbell
+
+    end
+
+
+    def dumbbell
+
+      ______                  ______
+     |      |			     |      |
+  ___|      |			     |      |____
+ |   |      |                | 	    |    |
+ |   |      |________________|      |    | 
+ |   | 25L  |________________| 25LB |    |
+ |   |      |                |      |    |
+ |___|      |                |      |____|
+     |	    |                |      |
+     |______|                |______|  
+
+    end
+
+end
