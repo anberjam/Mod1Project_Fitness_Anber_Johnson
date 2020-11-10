9318a8939610ce6cd737e51274af69ccfed4c5b5
jyliu17 <jyliu17@gmail.com>
2020-11-10 12:11:28 -0500

    login menu

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -10,18 +10,54 @@ class CLI
     end
 
 
-    def dumbbell
+    def self.main_menu
+        prompt = TTY::Prompt.new
+        display_menu = prompt.select ("Are you a returning member or a new member?")
 
+
+    end
+
+    def self.create_account
+        prompt = TTY::Prompt.new
+        username = prompt.ask("Please enter your gains name:")
+        password = prompt.mask("Protect your gains with a password:")
+        @user = User.create(username: username, password: password)
+        system('clear')
+        self.login
+    end
+
+    def self.login
+        prompt = TTY::Prompt.new
+        username = prompt.ask("Please enter your gains name:")
+        password = prompt.mask("Access your gains with your password:")
+        @user = User.find_by(username: username, password: password)
+        if @user
+
+        # @Workout_plans <-- need to build method
+        else
+            puts "These are not your gains, please try again."
+            system
+        end
+
+    end
+
+
+
+
+    def dumbbell
+   "
       ______                  ______
      |      |			     |      |
   ___|      |			     |      |____
  |   |      |                | 	    |    |
  |   |      |________________|      |    | 
- |   | 25LB |________________| 25LB |    |
+ |   |      |________________|      |    |
  |   |      |                |      |    |
  |___|      |                |      |____|
      |	    |                |      |
      |______|                |______|  
+     
+     "
 
     end
 
