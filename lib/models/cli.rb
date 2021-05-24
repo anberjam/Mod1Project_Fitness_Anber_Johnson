78c1a0df91b9a1c42c7ed572947e62b9ae48f29d
anberjamz <anberjam@yahoo.com>
2021-05-24 19:46:41 -0400

    more formatting

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -164,13 +164,13 @@ class CLI
         prompt = TTY::Prompt.new
 
         muscle_group = prompt.select ("Which body part would you like to work out?") do |body_part|
-        body_part.choice "Legs"
-        body_part.choice "Arms"
-        body_part.choice "Chest"
-        body_part.choice "Shoulders"
-        body_part.choice "Back"
-        body_part.choice "Abs"
-        body_part.choice "I would like to return to the main menu"
+            body_part.choice "Legs"
+            body_part.choice "Arms"
+            body_part.choice "Chest"
+            body_part.choice "Shoulders"
+            body_part.choice "Back"
+            body_part.choice "Abs"
+            body_part.choice "I would like to return to the main menu"
         end
 
         if muscle_group == "Legs"
